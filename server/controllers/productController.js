b05dec22eaea48f89e601237a1f490f26c7ea577
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-04-03 22:02:13 +0530

    Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,22 +1,52 @@
 // controllers/productController.js
 const db = require('../models');
 
+// Function to fetch the last Code from the database
+const getLastCode = async () => {
+  try {
+    const query = `
+      SELECT MAX(Code) AS lastCode FROM Products
+    `;
+    const result = await db.sequelize.query(query, { type: db.sequelize.QueryTypes.SELECT });
+    return result[0].lastCode || 0; // Return the lastCode or 0 if no products exist yet
+  } catch (error) {
+    console.error('Error fetching last product code:', error);
+    throw error;
+  }
+};
+
 exports.createProduct = async (req, res) => {
   try {
-    const { Code, Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice } = req.body;
+    const { Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice, createdAt, updatedAt } = req.body;
+
+    // Fetch the last Code from the database
+    let lastCode = await getLastCode();
+
+    // Increment the last Code by 1 to get the new Code for the new product
+    let newCode = lastCode + 1;
+
+    let modifiedCreatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    let modifiedUpdatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+
+    // Insert the new product with the newCode
     const query = `
-      INSERT INTO Products (Code, Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      INSERT INTO Products (Code, Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice,UserID,createdAt,updatedAt )
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
     const result = await db.sequelize.query(query, {
-      replacements: [Code, Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice],
+      replacements: [newCode, Name, TypeID, CategoryID, BrandID, ColourID, Cost, SellingPrice, null, modifiedCreatedAt, modifiedUpdatedAt],
       type: db.sequelize.QueryTypes.INSERT
     });
+
+    // Fetch the newly inserted product
     const newProductId = result[0];
     const newProduct = await db.sequelize.query('SELECT * FROM Products WHERE Code = ?', {
       replacements: [newProductId],
       type: db.sequelize.QueryTypes.SELECT
     });
+
+    // Respond with the newly inserted product
     res.status(201).json(newProduct[0]);
   } catch (error) {
     console.error('Error creating product:', error);
