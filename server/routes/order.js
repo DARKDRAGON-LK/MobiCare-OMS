5f753d68dc7e15dac9ef6d6cbdd9746b119b75a2
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-04-13 19:18:24 +0530

    Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

---
+++ server/routes/order.js
@@ -0,0 +1,13 @@
+// routes/orderRoutes.js
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+// Define routes
+router.get('/orders', orderController.getAllOrders);
+router.get('/orders/:id', orderController.getOrderById);
+router.post('/orders', orderController.createOrder);
+router.put('/orders/:id', orderController.updateOrder);
+router.delete('/orders/:id', orderController.deleteOrder);
+
+module.exports = router;
