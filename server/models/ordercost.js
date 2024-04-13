e36725a055940e6c1379f4315565e20d3a9386fe
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-04-13 19:18:11 +0530

    Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- server/models/ordercost.js
+++ server/models/ordercost.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
     const OrderCost = sequelize.define('OrderCost', {
-      Code: Sequelize.INTEGER,
+      OrderID: Sequelize.INTEGER,
       DeliveryCost: Sequelize.DECIMAL(10, 2),
       AdditionalCost: Sequelize.DECIMAL(10, 2),
       UserID: Sequelize.INTEGER
     });
   
-    OrderCost.belongsTo(require('./product')(sequelize, Sequelize), { foreignKey: 'Code' });
+    OrderCost.belongsTo(require('./order')(sequelize, Sequelize), { foreignKey: 'OrderID' });
   
     return OrderCost;
   };
