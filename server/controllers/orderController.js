b9c6c645f129245ce64977c3acc0601329a522b0
Lawan Samarasekara <lawan.chaamindu1234@gmail.com>
2024-04-16 05:54:35 +0530

    Signed-off-by: Lawan Samarasekara <lawan.chaamindu1234@gmail.com>

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -31,6 +31,7 @@ const createOrder = async (req, res) => {
     const order = await Order.create({ SaleID, Date, CustomerID, SocialMediaPlatform, ShippedDate, Status, TrackingNumber, PaymentType, UserID });
     res.status(201).json(order);
   } catch (error) {
+    console.log(error);
     res.status(400).json({ message: error.message });
   }
 };
@@ -74,10 +75,24 @@ const deleteOrder = async (req, res) => {
   }
 };
 
+const getLastSaleID = async (req, res) => {
+  try {
+    const lastOrder = await Order.findOne({ order: [['createdAt', 'DESC']] });
+    if (!lastOrder) {
+      return res.status(404).json({ message: 'No orders found' });
+    }
+    const nextID = lastOrder.SaleID + 1;
+    res.json({ lastSaleID: nextID });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllOrders,
   getOrderById,
   createOrder,
   updateOrder,
   deleteOrder,
+  getLastSaleID,
 };
